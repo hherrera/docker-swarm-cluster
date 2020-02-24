6baa928184e0d7bb1eb2ee1c8169d3451cde5ac6
Hernando Herrera <hherrera@MacBook-Pro-de-Hernando.local>
2020-02-24 16:50:39 -0500

    path domain2

--- initialize-services.sh
+++ initialize-services.sh
@@ -7,7 +7,7 @@ echo "CREATING INGRESS SERVICES STACK..."
 export $(cat .env) && docker stack deploy --compose-file docker-compose-ingress.yml ingress
 
 echo "CREATING ADMINISTRATION SERVICES STACK..."
-#export $(cat .env) && docker stack deploy --compose-file docker-compose-admin.yml admin
+export $(cat .env) && docker stack deploy --compose-file docker-compose-admin.yml admin
 
 echo "CREATING METRICS SERVICES STACK..."
 #export $(cat .env) && docker stack deploy --compose-file docker-compose-metrics.yml metrics
